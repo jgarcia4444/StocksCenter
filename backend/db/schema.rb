9dbcdf1b9c4560c61b6875d0568f03aba134d836
github username <bballwiz4@gmail.com>
2020-09-17 20:12:05 -0700

    user_stocks table migrated with apropriate attributes (user_id, stock_symbol)

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_025807) do
+ActiveRecord::Schema.define(version: 2020_09_18_030737) do
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "stock_symbol"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
