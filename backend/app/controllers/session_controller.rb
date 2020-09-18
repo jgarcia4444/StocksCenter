f14062058653370b542d2d7c03c78939e4ba03f8
github username <bballwiz4@gmail.com>
2020-09-18 11:06:27 -0700

    User signup route correctlt receives mock data from frontend and sends user infor to back end to be processed and persisted in the database.

--- backend/app/controllers/session_controller.rb
+++ backend/app/controllers/session_controller.rb
@@ -1,11 +1,20 @@
 class SessionController < ApplicationController
 
     def signup
-        puts 'signed up!!!!!'
+        puts 'Users Controller Reached!!'
+        puts '--------------------------'
+        User.create(user_params)
+        users = User.all
+        render :json => users
     end
 
     def login 
         puts 'logged in !!!!!!!!'
     end
 
+
+    private 
+        def user_params
+            params.require(:session).permit(:first_name, :last_name, :email, :password)
+        end
 end
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    let options = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        first_name: "Test",
+        last_name: "User",
+        password: "password",
+        email: "test@user.com"
+      }),
+    }
+
+    fetch("http://localhost:3000/signup", options)
+      .then(res => res.json)
+      .then(data => console.log(data))
+
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Test!!!!!!!!!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
