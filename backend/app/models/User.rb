19ed48ebcd3aa5c7a52c688e7edefd434b22a361
github username <bballwiz4@gmail.com>
2020-09-18 18:36:39 -0700

    User model validations added for the first_name attribute. The firstname must three characters in length as well as only be comprised of letters.

--- backend/app/models/User.rb
+++ backend/app/models/User.rb
@@ -2,4 +2,21 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :user_stocks
     has_many :user_videos
+
+    validates :email, :uniqueness => true
+    validates :first_name, :presence => true 
+    validates :first_name, :length => { minimum: 3 }
+    validates_with FirstNameAllLettersValidator
+end
+
+class FirstNameAllLettersValidator < ActiveModel::Validator
+    def validate(record)
+
+        first_name = record.first_name
+        first_name.each do |char|
+            if char.to_i != 0
+                record.errors[:first_name_contains_nums] << "The first name must not contain numbers."
+            end
+        end
+    end
 end
