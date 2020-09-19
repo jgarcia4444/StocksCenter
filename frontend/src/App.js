89761c265ffb1fd48c44677a96400e13967ebbc3
github username <bballwiz4@gmail.com>
2020-09-18 18:13:05 -0700

    Unneeded testing code removed from app.js and from app.css.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,6 @@ import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-
-    fetch("http://localhost:3000/users/3", {
-      method: 'GET',
-      headers: {
-        'Content-Type': "application/json",
-        'Accept': 'application/json'
-      },
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-
-  }
   
   render() {
     return (
