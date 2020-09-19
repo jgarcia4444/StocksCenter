68584b234f71c4f2a278dc675373ae9caaa5d300
github username <bballwiz4@gmail.com>
2020-09-18 21:15:37 -0700

    validations added for last_name.

--- backend/app/models/User.rb
+++ backend/app/models/User.rb
@@ -6,17 +6,7 @@ class User < ActiveRecord::Base
     validates :email, :uniqueness => true
     validates :first_name, :presence => true 
     validates :first_name, :length => { minimum: 3 }
-    validates_with FirstNameAllLettersValidator
-end
+    validates :last_name, :presence => true
+    validates :last_name, :length => { minimum: 3 }
 
-class FirstNameAllLettersValidator < ActiveModel::Validator
-    def validate(record)
-
-        first_name = record.first_name
-        first_name.each do |char|
-            if char.to_i != 0
-                record.errors[:first_name_contains_nums] << "The first name must not contain numbers."
-            end
-        end
-    end
 end
