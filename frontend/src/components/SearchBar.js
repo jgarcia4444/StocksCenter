395813469f816733c73621935489cea84d2eebdd
github username <bballwiz4@gmail.com>
2020-09-21 11:53:15 -0700

    padding readded to the body.

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -9,7 +9,7 @@ export default class SearchBar extends Component {
                 <div className="row form-group">
                     <div className="col-8">
                         <input name="searchBarText" type="text" onChange={(e) => this.props.setSearchQuery(e)} className="form-control" />
-                        <small>Search by stock symbol.</small>
+                        <small>Search by company name.</small>
                     </div>
                     <div className="col-4">
                         <button className="form-control" type="Submit">Search</button>
