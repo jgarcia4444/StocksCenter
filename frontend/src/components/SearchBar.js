bd8e5d0e6b451d438dfafde8e517bb9b24819700
github username <bballwiz4@gmail.com>
2020-09-21 11:50:38 -0700

    Fix added to background gradient to work with the differing sizes of the page.

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -5,7 +5,7 @@ export default class SearchBar extends Component {
 
     render() {
         return (
-           <form onSubmit={this.props.fetchSearchData}>
+           <form autocomplete="off" onSubmit={this.props.fetchSearchData}>
                 <div className="row form-group">
                     <div className="col-8">
                         <input name="searchBarText" type="text" onChange={(e) => this.props.setSearchQuery(e)} className="form-control" />
