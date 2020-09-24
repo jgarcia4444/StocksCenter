0ca458adf0cb76261894175cc09c13b127d34b1e
github username <bballwiz4@gmail.com>
2020-09-24 13:49:14 -0700

    Form added and ready to send a fetch to the backend to save a user.

--- frontend/src/components/auth/signup/SignupForm.js
+++ frontend/src/components/auth/signup/SignupForm.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
 const SignupForm = (props) => {
+
     return (
-        <div className="signup-form">
-            This will be the signup form
+        <div className="signup-form container">
+            <div className="row">
+                <div className="col-12">
+                  <h2>Signup Form</h2>  
+                </div>
+            </div>
+            <form onSubmit={props.handleFormSubmit}>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name</label>
+                            <input name="firstName" type="text" className="form-control" onChange={ props.handleInputChange}/>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="form-group">
+                            <label htmlFor="lastName">Last Name</label>
+                            <input type="text" name="lastName" className="form-control" onChange={ props.handleInputChange}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" className="form-control" name="email" onChange={ props.handleInputChange}/>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" className="form-control" aria-describedby="passwordHelp" onChange={ props.handleInputChange}/>
+                            <small id="passwordHelp" className="form-text" style={{color : props.passwordNoteColor}}>Password must be 8 characters long.</small>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Signup</button>
+            </form>
         </div>
     )
 }
--- frontend/src/components/auth/signup/SignupPageContainer.js
+++ frontend/src/components/auth/signup/SignupPageContainer.js
@@ -8,14 +8,36 @@ class SignupPageContainer extends Component {
         firstName: "",
         lastName: "",
         password: "",
-        email: ""
+        email: "",
+        passwordNoteColor: "red"
+    }
+
+    handleInputChange = (e) => {
+        if (e.target.name === "password" && e.target.value.length > 7) {
+            this.setState({
+                ...this.state,
+                [e.target.name]: e.target.value,
+                passwordNoteColor: "green"
+            })
+        } else {
+            this.setState({
+                ...this.state,
+                [e.target.name]: e.target.value,
+                passwordNoteColor: "red"
+            })
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.value)
     }
 
     render() {
 
         return (
             <div className="Home">
-                <SignupForm handleFormSubmit={this.handleSubmit} handleInputChange={this.handleInputChange} />
+                <SignupForm passwordNoteColor={this.state.passwordNoteColor} handleFormSubmit={(e) => this.handleSubmit(e)} handleInputChange={(e) => this.handleInputChange(e)} />
             </div>
         )
 
