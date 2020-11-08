8b0e46062f1b69bd593c5e3b36b71e83f7b3e9d0
github username <bballwiz4@gmail.com>
2020-11-08 14:08:02 -0800

    video conatainer set uup to handle the data being given to it and display it laid out.

--- frontend/src/components/videos/VideoContainer.js
+++ frontend/src/components/videos/VideoContainer.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import './VideoContainer.css';
 
 class VideoContainer extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            // Liked state tracker.
+        }
+    }
+
     render() {
 
+        const { id, snippet } = this.props.video
+
         return (
-            <div className="video-container">
-                Hello World
+            <div className="row video-container">
+                <div classnName="col-12">
+                    <div className="row video-title-row">
+                        <div className="col-12 video-title">
+                            {snippet.title}
+                        </div>
+                    </div>
+                    <div className="row video-description-row">
+                        <div className="col-12 video-description">
+                            {snippet.description}
+                        </div>
+                    </div>
+                    <div className="row video-thumbnail-row">
+                        <div className="col-12 video-thumbnail">
+                            <img src={snippet.default.url}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
